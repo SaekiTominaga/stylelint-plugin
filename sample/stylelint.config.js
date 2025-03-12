4cbcecc0923760d4f0d9c12806e263b6b41eeade
SaekiTominaga <kcsu3470+github@gmail.com>
2025-03-12 19:42:10 +0900

    順序や変数名の変更（挙動に影響なし）

--- sample/stylelint.config.js
+++ sample/stylelint.config.js
@@ -1,21 +1,18 @@
 import pluginDisplayMultiKeyword from '../packages/display-multi-keyword/dist/index.js';
+import pluginNoDefaultViewport from '../packages/no-default-viewport/dist/index.js';
 import pluginRootColors from '../packages/root-colors/dist/index.js';
-import pluginViewportExplicit from '../packages/no-default-viewport/dist/index.js';
 
 /** @type {import('stylelint').Config} */
 export default {
-	plugins: [pluginDisplayMultiKeyword, pluginRootColors, pluginViewportExplicit],
+	plugins: [pluginDisplayMultiKeyword, pluginNoDefaultViewport, pluginRootColors],
 	rules: {
-		/* [plugin] stylelint-display-multi-keyword*/
+		/* [plugin] display-multi-keyword*/
 		'plugin/display-multi-keyword': [
 			true,
 			{
 				severity: 'warning',
 			},
 		],
-
-		/* [plugin] stylelint-root-colors */
-		'plugin/root-colors': true,
 
 		/* [plugin] no-default-viewport */
 		'plugin/no-default-viewport': [
@@ -24,5 +21,8 @@ export default {
 				severity: 'warning',
 			},
 		],
+
+		/* [plugin] root-colors */
+		'plugin/root-colors': true,
 	},
 };
