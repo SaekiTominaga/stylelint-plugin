9e3c8ace0f5097d7bb7633a234ebfcc6e043b302
SaekiTominaga <kcsu3470+github@gmail.com>
2025-12-14 00:52:49 +0900

    エラーサンプルに attribute-case-sensitivity の例を追加

--- sample/stylelint.config.js
+++ sample/stylelint.config.js
@@ -1,12 +1,16 @@
+import pluginAttributeCaseSensitivity from '../packages/attribute-case-sensitivity/dist/index.js';
 import pluginDisplayMultiKeyword from '../packages/display-multi-keyword/dist/index.js';
 import pluginNoDefaultViewport from '../packages/no-default-viewport/dist/index.js';
 import pluginRootColors from '../packages/root-colors/dist/index.js';
 
 /** @type {import('stylelint').Config} */
 export default {
-	plugins: [pluginDisplayMultiKeyword, pluginNoDefaultViewport, pluginRootColors],
+	plugins: [pluginAttributeCaseSensitivity, pluginDisplayMultiKeyword, pluginNoDefaultViewport, pluginRootColors],
 	rules: {
-		/* display-multi-keyword*/
+		/* attribute-case-sensitivity */
+		'plugin/attribute-case-sensitivity': true,
+
+		/* display-multi-keyword */
 		'plugin/display-multi-keyword': true,
 
 		/* no-default-viewport */
